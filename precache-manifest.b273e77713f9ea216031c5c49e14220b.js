21068207d0647c513cb2513235dd12d912d5019e
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-07-09 01:03:40 +0300

    Updates

---
+++ precache-manifest.b273e77713f9ea216031c5c49e14220b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53dfc81e4fb5045398e793376d9f9045",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c9bf5dac697742c7c283",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.e521ba9b.chunk.css"
+  },
+  {
+    "revision": "8245ffa9e637d99ef907",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.989b4b36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.989b4b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9bf5dac697742c7c283",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.2591b74c.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
